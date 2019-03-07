34ff0ba210d32483c547334b193f69687cb3e5d2
Shahrukhkhan86 <shahrukh.khan_mca18@gla.ac.in>
2019-03-07 10:56:35 +0530

    add edittext and button in java file

--- app/src/main/java/com/example/a7march2/MainActivity.java
+++ app/src/main/java/com/example/a7march2/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.a7march2;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edit1, edit2;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
