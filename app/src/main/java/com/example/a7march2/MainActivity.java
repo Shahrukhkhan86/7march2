aac191b2278545e67d90251374b17d11112d5d50
Shahrukhkhan86 <shahrukh.khan_mca18@gla.ac.in>
2019-03-07 11:25:58 +0530

    add Toast

--- app/src/main/java/com/example/a7march2/MainActivity.java
+++ app/src/main/java/com/example/a7march2/MainActivity.java
@@ -4,12 +4,16 @@ import android.app.backup.SharedPreferencesBackupHelper;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText edit1, edit2;
     Button button;
+    String s1;
+    String s2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,21 @@ public class MainActivity extends AppCompatActivity {
         edit1=findViewById(R.id.edit1);
         edit2=findViewById(R.id.edit2) ;
         button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user =edit1.getText().toString().trim();
+                String pass =edit2.getText().toString().trim();
+                if(user.equals(s1)&& pass.equals(s2))
+                {
+                    Toast.makeText(MainActivity.this, "Successfull", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+
+                }
+            }
+        });
 
     }
 
@@ -26,6 +45,10 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         SharedPreferences sharedPreferences=getSharedPreferences("com.example.a7march2",MODE_PRIVATE);
         SharedPreferences.Editor editor=sharedPreferences.edit();
+        editor.putString("username","shahrukh.khan_mca18@gla.ac.in");
+        editor.putString("password","12345");
+
+
 
 
     }
