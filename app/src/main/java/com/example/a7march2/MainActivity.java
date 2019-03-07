acc27410413ead2773b4be456ef161eb9d628bf5
Shahrukhkhan86 <shahrukh.khan_mca18@gla.ac.in>
2019-03-07 11:13:57 +0530

    commit

--- app/src/main/java/com/example/a7march2/MainActivity.java
+++ app/src/main/java/com/example/a7march2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a7march2;
 
+import android.app.backup.SharedPreferencesBackupHelper;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edit1=findViewById(R.id.edit1);
+        edit2=findViewById(R.id.edit2) ;
+        button=findViewById(R.id.button);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences sharedPreferences=getSharedPreferences("com.example.a7march2",MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+
+
     }
 }
